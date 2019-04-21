f3c97dba0b9c09172cc1615acc369991340312fe
Xiuyuan Yu <yuxiuyuan0324@gmail.com>
2019-04-21 10:28:02 -0400

    add components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FoodCollectionComponent } from './food-collection/food-collection.component';
+import { AddFoodComponent } from './add-food/add-food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FoodCollectionComponent,
+    AddFoodComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/src/app/food-collection/food-collection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-collection',
+  templateUrl: './food-collection.component.html',
+  styleUrls: ['./food-collection.component.css']
+})
+export class FoodCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
